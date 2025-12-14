eecf442208924d45959966a68f5f42e4be130d85
Mohit Negi <mnegi1433@gmail.com>
2025-12-14 19:50:59 +0530

    ch4 l3 completed

--- internal/request/request.go
+++ internal/request/request.go
@@ -36,40 +36,45 @@ func checkUpperCase(str string) bool {
 }
 
 func parseRequestLine(req string) (*RequestLine, int, error) {
-	req_line := strings.Split(req, "\r\n")[0]
-	if req_line == "" {
+	idx := strings.Index(req, "\r\n")
+
+	if idx == -1 {
 		return nil, 0, nil
 	}
+
+	req_line := strings.Split(req, "\r\n")[0]
 	parts := strings.Split(req_line, " ")
 
-	if len(parts) != 3 {
-		return nil, 0, fmt.Errorf("invalid request line")
-	}
-
-	method, target, protocol_version := parts[0], parts[1], parts[2]
+	method, target, version := parts[0], parts[1], parts[2]
 
 	if !checkUpperCase(method) {
 		return nil, 0, fmt.Errorf("method in request line should be uppercase")
 	}
 
-	version := strings.Split(protocol_version, "/")[1]
-	if version != "1.1" {
+	// version := strings.Split(protocol_version, "/")[1]
+	if version != "HTTP/1.1" {
 		return nil, 0, fmt.Errorf("%s: incorrect protocol version. 1.1 supported", version)
 	}
 
-	res := RequestLine{HttpVersion: version, RequestTarget: target, Method: method}
+	res := RequestLine{HttpVersion: "1.1", RequestTarget: target, Method: method}
 	return &res, len(req_line), nil
 }
 
 func (r *Request) parse(data []byte) (int, error) {
-	r.ParserStatus = INITIALIZED
 
 	requestLine, parsedBytes, err2 := parseRequestLine(string(data))
 	if err2 != nil {
 		return 0, errors.Join(fmt.Errorf("some error occured while parsing line: "), err2)
 	}
 
-	r.RequestLine = *requestLine
+	if parsedBytes == 0 {
+		return 0, nil
+	}
+
+	r.ParserStatus = DONE
+	if requestLine != nil {
+		r.RequestLine = *requestLine
+	}
 	return parsedBytes, nil
 }
 
@@ -77,12 +82,21 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 
 	request := Request{}
 	buff := make([]byte, 1024)
-	for {
-		req, err := io.ReadAtLeast(reader, buff, 8)
+	bufLen := 0
+	for request.ParserStatus != DONE {
+		n, err := reader.Read(buff[bufLen:])
 		if err != nil {
 			return nil, err
 		}
 
+		bufLen += n
+		readBytes, err2 := request.parse(buff[:bufLen])
+		if err2 != nil {
+			return nil, err2
+		}
+
+		copy(buff, buff[readBytes:bufLen])
+		bufLen -= readBytes
 	}
 	return &request, nil
 }
